df7819f13ac15a9794388cd4d08e9ef6826c51b7
Lloyd Wang <LWANG046@e.ntu.edu.sg>
2023-10-02 21:06:23 +0800

    add hooks and ci.yml

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      files: ['**/*.{js,jsx,ts,tsx}'],
+      extends: ['plugin:prettier/recommended'],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: { 'prettier/prettier': ['error', { singleQuote: true }] },
+};
