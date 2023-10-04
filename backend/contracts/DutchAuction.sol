25b43bcbe11b1a9e5544865ced144eb5688020d7
Richard <feavfas@gmail.com>
2023-10-04 23:07:41 +0800

    added deploy.js

--- backend/contracts/DutchAuction.sol
+++ backend/contracts/DutchAuction.sol
@@ -4,33 +4,18 @@ import "./Queue.sol";
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
-
     function balanceOf(address account) external view returns (uint256);
-
-    function transfer(
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
-    function allowance(
-        address owner,
-        address spender
-    ) external view returns (uint256);
-
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
 function min(uint256 a, uint256 b) pure returns (uint256) {
     return a <= b ? a : b;
 }
 
-contract dutchAuction {
+contract DutchAuction {
     uint private constant DURATION = 20 minutes;
 
     IERC20 public immutable token;
@@ -40,51 +25,90 @@ contract dutchAuction {
     address payable public immutable seller;
     uint public immutable startingPrice;
     uint public immutable discountRate;
-    uint public immutable startAt;
-    uint public immutable expiresAt;
+    uint public startAt;
+    uint public expiresAt;
+    bool public active;
 
     uint private tokenNetWorthPool;
     uint private currentBidNetWorthPool;
-    Queue private bidQueue;
+    Queue private bidQueue; 
 
+    event AuctionCreated(address indexed _seller, address indexed _token, uint _qty, uint startPrice, uint discountRate);
     event StartOfAuction();
     event DespositTokens(address indexed _from, uint indexed _qty);
     event LogBid(address indexed _from, uint indexed _price);
     event EndOfAuction(uint indexed _time, uint _qtySold, uint _salePrice);
-    event SuccessfulBid(
-        address indexed _bidder,
-        uint _qtyAlloacated,
-        uint refund
-    );
+    event SuccessfulBid(address indexed _bidder, uint _qtyAlloacated, uint refund);
+
+    modifier onlyNotSeller {
+        require(msg.sender != seller, "The seller cannot perform this action");
+        _;
+    }
+
+    modifier onlySeller {
+        require(msg.sender == seller, "Only the seller can perform this action");
+        _;
+    }
+
+    modifier onlyAfterStart {
+        require(block.timestamp >= startAt, "This auction has not started");
+        _;
+    }
+
+    modifier onlyBeforeEnd {
+        require(block.timestamp < expiresAt, "This auction has ended");
+        _;
+    }
+
+    modifier onlyActive {
+        require(active, "This auction is no longer active");
+        _;
+    }
+
+    modifier onlyNotActive {
+        require(!active, "This auction is already active");
+        _;
+    }
 
     constructor(
+        address _seller,
         uint _startingPrice,
         uint _discountRate,
         address _token,
         uint _tokenQty,
         uint _tokenId
     ) {
-        seller = payable(msg.sender);
+        seller = payable(_seller);
         startingPrice = _startingPrice;
         discountRate = _discountRate;
-        startAt = block.timestamp;
-        expiresAt = block.timestamp + DURATION;
 
-        require(
-            _startingPrice >= _discountRate * DURATION,
-            "Starting price is too low"
-        );
+        require(_startingPrice >= _discountRate * DURATION, "Starting price is too low");
 
         token = IERC20(_token);
         tokenQty = _tokenQty;
         tokenId = _tokenId;
-        console.log(tokenQty, msg.sender, token.balanceOf(msg.sender));
 
         tokenNetWorthPool = startingPrice * tokenQty;
-        bidQueue = new Queue(); //TODO: start auction only after tokens have been injected
+        bidQueue = new Queue();
+        
+        emit AuctionCreated(seller, _token, tokenQty, startingPrice, discountRate);
     }
-
-    function injectTokens() external {
+    
+    function startAuction() 
+        onlySeller
+        onlyNotActive
+        external {
+            injectTokens();
+            require(tokenQty == token.balanceOf(address(this)), "Not enough tokens injected");
+            startAt = block.timestamp;
+            expiresAt = block.timestamp + DURATION;
+            active = true;
+            emit StartOfAuction();
+    }
+    function injectTokens() 
+        onlySeller
+        onlyNotActive
+        internal {
         token.transferFrom(msg.sender, address(this), tokenQty);
         emit DespositTokens(msg.sender, tokenQty); //TODO: add tons of checks + tests
     }
@@ -95,52 +119,59 @@ contract dutchAuction {
         return startingPrice - discount;
     }
 
-    function getCurrentTokenNetWorth() private view returns (uint) {
+    function getCurrentTokenNetWorth() internal view returns (uint) {
         uint currentPrice = getPrice();
         return currentPrice * tokenQty;
     }
 
-    function placeBid() external payable {
-        require(block.timestamp < expiresAt, "This auction has ended");
+    function placeBid() 
+        onlyAfterStart
+        onlyBeforeEnd
+        onlyNotSeller
+        onlyActive
+        external payable{
+            require(block.timestamp < expiresAt, "This auction has ended");
 
-        address bidder = msg.sender;
-        uint bidValue = msg.value;
-        uint price = getPrice();
-        require(
-            bidValue >= price,
-            "The amount of ETH sent is less than the price of token"
-        );
+            address bidder = msg.sender;
+            uint bidValue = msg.value;
+            uint price = getPrice();
 
-        bidQueue.enqueue(bidder, bidValue);
-        uint currentBidPool = bidQueue.currentBidPool();
-        uint currentTokenNetWorth = getCurrentTokenNetWorth();
-        if (currentBidPool >= currentTokenNetWorth) {
-            endAuction(currentTokenNetWorth);
-        }
-        // selfdestruct(seller); //TODO: what is the new opcode?
+            require(bidValue >= price, "The amount of ETH sent is less than the price of token");
+            
+            bidQueue.enqueue(bidder, bidValue);
+            emit LogBid(bidder, price);
+
+            uint currentBidPool = bidQueue.currentBidPool();
+            uint currentTokenNetWorth = getCurrentTokenNetWorth();
+            if (currentBidPool >= currentTokenNetWorth) {
+                active = false;
+                endAuction(currentTokenNetWorth);
+            }
     }
 
-    function endAuction(uint tokenValue) internal {
+    function endAuction(uint tokenValue) 
+        onlyAfterStart
+        onlyNotActive 
+        internal{
         address bidder;
         uint bidValue;
         uint pricePerToken = tokenValue / tokenQty;
 
         uint tokensUnallocated = tokenQty;
-        while (tokensUnallocated > 0) {
+        while (bidQueue.length() > 0 && tokensUnallocated > 0) {
             (bidder, bidValue) = bidQueue.dequeue();
-            uint qtyAllocated = min(
-                tokensUnallocated,
-                bidValue / pricePerToken
-            );
-            uint remainder = bidValue - qtyAllocated * pricePerToken;
+            uint qtyAllocated = min(tokensUnallocated, bidValue / pricePerToken);
+            uint remainder = bidValue - qtyAllocated*pricePerToken;
             tokensUnallocated -= qtyAllocated;
 
             token.transfer(bidder, qtyAllocated);
             if (remainder > 0) {
                 payable(bidder).transfer(remainder);
             }
+            emit SuccessfulBid(bidder, qtyAllocated, remainder);
+
         }
-        console.log("seller transfered ", address(this).balance);
         seller.transfer(address(this).balance);
+        emit EndOfAuction(block.timestamp, tokenQty - tokensUnallocated, pricePerToken);
     }
 }
--- backend/contracts/Queue.sol
+++ backend/contracts/Queue.sol
@@ -1,19 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 struct Bid {
     address bidder;
     uint bidValue;
 }
 
 contract Queue {
-    mapping(uint256 => Bid /* or any other type */) public _queue;
+
+    mapping (uint256 => Bid) public _queue;
     uint256 public first;
     uint256 public last;
     uint256 public bidPool;
-
     constructor() {
         first = 1;
         last = 1;
@@ -22,7 +20,6 @@ contract Queue {
 
     function enqueue(address _bidAddr, uint _bidValue) public {
         _queue[last] = Bid(_bidAddr, _bidValue);
-        console.log("enqueu", last, _queue[last].bidder, _queue[last].bidValue);
         last += 1;
         bidPool += _bidValue;
     }
@@ -30,7 +27,6 @@ contract Queue {
     function dequeue() public returns (address, uint) {
         Bid memory bid;
         require(last > first);
-        console.log("deq", first, _queue[first].bidder, _queue[first].bidValue);
         bid = _queue[first];
         delete _queue[first];
         address addr = bid.bidder;
